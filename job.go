56fb221d6948aedd7796a68e73ab0a6e5aab9320
denis <sedovdv16@yandex.ru>
2026-09-17 18:58:35 +0300

    feat: add initial Job model

---
+++ job.go
@@ -0,0 +1,19 @@
+package main
+
+import "time"
+
+type JobStatus string
+
+const (
+	JobStatusPending   JobStatus = "pending"
+	JobStatusRunning   JobStatus = "running"
+	JobStatusCompleted JobStatus = "completed"
+	JobStatusFailed    JobStatus = "failed"
+)
+
+type Job struct {
+	ID        int
+	Name      string
+	Status    JobStatus
+	CreatedAt time.Time
+}
